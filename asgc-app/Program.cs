315ce82a67c392d46f527fa6128f3479c4cbe8d0
n3mo1101 <rd.cruz1101@gmail.com>
2024-09-20 21:35:39 +0800

    ASGC-App

---
+++ asgc-app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASGC_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
