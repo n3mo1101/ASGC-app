6d0eb6644c57b6ba1935818dfc85aa305d89c61d
Kert-Ace T. Ombion <155365666+acesuus@users.noreply.github.com>
2024-09-20 21:45:20 +0800

    Main code

--- asgc-app/Program.cs
+++ asgc-app/Program.cs
@@ -1,12 +1,67 @@
 ﻿using System;
+using System.Linq;
 
-namespace ASGC_APP
+class StudentRecord
 {
-    class Program
+    public List<int> Assignment = new List<int>();
+    public List<int> Quizzes = new List<int>();
+    public int FinalExam = new int();
+
+    public StudentRecord(List<int> assignment, List<int> quizzes, int finalExam)
     {
-        static void Main(string[] args)
+        Assignment = assignment;
+        Quizzes = quizzes;
+        FinalExam = finalExam;
+    }
+}
+
+
+
+class ASGC
+{
+
+    static void Main(string[] args)
+    {
+        Dictionary<string, StudentRecord> Grades = new Dictionary<string, StudentRecord>();
+
+        Console.WriteLine("Enter student's name");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("How many assingments to compute?");
+        int Assnum = int.Parse(Console.ReadLine());
+
+        List<int> assignment = new List<int>();
+        for (int i = 1; i <= Assnum; i++)
         {
-            Console.WriteLine("Hello, world!");
+            Console.WriteLine($"Enter score for Assignment no.{i}: ");
+            assignment.Add(int.Parse(Console.ReadLine()));
         }
+
+        Console.WriteLine("How many quizzes to compute?");
+        int Quiznum = int.Parse(Console.ReadLine());
+
+        List<int> quizzes = new List<int>();
+        for (int i = 1; i <= Quiznum; i++)
+        {
+            Console.WriteLine($"Enter score for Quiz no.{i}: ");
+            quizzes.Add(int.Parse(Console.ReadLine()));
+        }
+
+        Console.WriteLine("Enter score for Final Exam: ");
+        int Final_Exam = int.Parse(Console.ReadLine());
+
+        StudentRecord student = new StudentRecord(assignment, quizzes, Final_Exam);
+        Grades[name] = student;
+
+    }
+
+    static double Calculate_Average(StudentRecord student_Grade)
+    {
+        double AssingmentWeight = student_Grade.Assignment.Average() * 0.3;
+        double QuizWeight = student_Grade.Quizzes.Average() * 0.3;
+        double FinalExam_Weight = student_Grade.FinalExam * 0.4;
+
+        double weighted_Average = AssingmentWeight + QuizWeight + FinalExam_Weight;
+        return weighted_Average;
     }
 }
